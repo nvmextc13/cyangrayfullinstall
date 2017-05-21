b584e570a3459e2042be8ed257b6ff6b61ebbebb
nvmextc13 <nvmextc@gmail.com>
2017-05-21 19:33:03 +1000

    May 21 2017

---
+++ 003copyconfig.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What to restore. 
+#.zsh_history
+#.bashrc
+#.bash_history
+
+backup_files="
+home/me/bin/
+home/me/.config/
+home/me/.oh-my-zsh/
+home/me/.zshrc
+"
+
+# Where to resore to.
+dest="$HOME"
+
+# Print start status message.
+echo "Restoring $backup_files to $dest"
+cp -R $backup_files $dest
+
+# Print end status message.
+echo
+echo "Restore finished"
